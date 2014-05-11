96e289dc88326facbda17abe31040b6459081dc0
anoopajay91 <anoopajay91@gmail.com>
2014-05-11 17:43:26 +0530

    Movie Info

---
+++ README.txt
@@ -0,0 +1,16 @@
+Dependencies:
+
+1.Python
+2.BeautifulSoup module
+3.Requests module
+
+The script will find all movies that have the specified keyword(input) and returns you 
+->Movie Name
+->Rating
+->Genre.
+
+Have fun :)
+
+
+
+
---
+++ movieInfo.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+
+prompt = raw_input("Enter movie name\n")
+result = requests.get("http://www.google.com/search?q="+prompt.replace(' ','+')+'+imdb')
+page = result.text.encode('ascii','ignore')
+result.close()
+
+soup = BeautifulSoup(page)
+element = soup.select("div cite")
+
+element = map(lambda x: str(x.text),element)
+imdbLinks = filter(lambda x: len(x.split('/'))==4 and x.split('.')[1]=='imdb',element)
+
+print str(len(imdbLinks))+" result(s) fetched"
+
+for each in imdbLinks:
+    result = requests.get("http://"+each)
+    page = result.text.encode('ascii','ignore')
+    result.close()
+    soup = BeautifulSoup(page)
+    name = soup.find("span",{'itemprop':'name'})
+    print "Movie is :"+name.text
+    rating = soup.find("div",{'class':'titlePageSprite star-box-giga-star'})
+    if rating:
+        print "Rating is :"+rating.text
+    else:
+        print "Rating not present"
+    genre = soup.findAll("span",{'class':'itemprop','itemprop':'genre'})
+    genre = map(lambda x:x.text,genre)
+    print 'Genre is :'+'|'.join(genre)
+    print
+    print
+    
